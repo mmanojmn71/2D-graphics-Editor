b1bcaf8b6f753b2518c7108bb226987997114b4e
Manoj M N <mmanojmn71@gmail.com>
2026-05-31 09:23:20 +0530

    Created canvas and display function

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
+char canvas[30][60];
+void initializeCanvas(){
+    for(int i=0;i<30;i++){
+        for(int j=0;j<60;j++){
+            canvas[i][j]=' ';
+        }
+    }
+}
+void displayCanvas(){
+    for(int i=0;i<30;i++){
+        for(int j=0;j<60;j++){
+            printf("%c",canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
 int main()
 
 {
-    printf("welcome to 2D Graphics Editor");
+    initializeCanvas();
+    displayCanvas();
+    getchar();
     return 0;
 }
