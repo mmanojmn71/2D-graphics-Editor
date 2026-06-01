ee4ef991f92bd59c9908a2be6d054605419c2b97
Manoj M N <mmanojmn71@gmail.com>
2026-06-02 00:05:20 +0530

    Created menu system

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 char canvas[30][60];
 void initializeCanvas();
+void clearCanvas();
 void displayCanvas();
 void drawLine(int row,int col,int length);
 void drawRectangle(int row,int col,int width,int height);
 void drawTriangle(int row,int col,int size);
 void drawCircle(int centerX,int centerY,int radius);
 void initializeCanvas(){
+    for(int i=0;i<30;i++){
+        for(int j=0;j<60;j++){
+            canvas[i][j]=' ';
+        }
+    }
+}
+void clearCanvas(){
     for(int i=0;i<30;i++){
         for(int j=0;j<60;j++){
             canvas[i][j]=' ';
@@ -60,12 +68,51 @@ void drawCircle(int centerX,int centerY,int radius){
 int main()
 
 {
-    initializeCanvas();
-    drawLine(2,5,10);
-    drawRectangle(8,10,12,5);
-    drawTriangle(15,5,8);
-    drawCircle(15,45,4);
-    displayCanvas();
-    getchar();
-    return 0;
+   int choice;
+   initializeCanvas();
+   do{
+    printf("\n===== 2D Graphics Editor =====\n");
+    printf("1 Draw Line\n");
+    printf("2 Draw Rectangle\n");
+    printf("3 Draw Triangle\n");
+    printf("4 Draw Circle\n");
+    printf("5 Display Canvas\n");
+    printf("6 Clear Canvas\n");
+    printf("7 exit\n");
+    printf("Enter Choice\n");
+    scanf("%d",&choice);
+    switch(choice){
+        case 1:
+        drawLine(2,5,10);
+        printf("Line Drawn\n");
+        break;
+        case 2:
+        drawRectangle(8,10,12,5);
+        printf("Rectangle Drawn\n");
+        break;
+        case 3:
+        drawTriangle(15,5,8);
+        printf("Triangle Drawn\n");
+        break;
+        case 4:
+        drawCircle(15,45,4);
+        printf("Circle Drawn\n");
+        break;
+        case 5:
+        displayCanvas();
+        break;
+        case 6:
+        clearCanvas();
+        printf("Canvas Cleared\n");
+        break;
+        case 7:
+        printf("Exiting Program...\n");
+        break;
+        default:
+        printf("invalid choice\n");
+    }
+
+   }
+   while(choice!=7);
+   return 0;
 }
