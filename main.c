a343421239f30dba3496d6f94f330e919984ad53
Manoj M N <mmanojmn71@gmail.com>
2026-06-01 12:40:39 +0530

    added triangle and circle drawing

--- main.c
+++ main.c
@@ -4,6 +4,8 @@ void initializeCanvas();
 void displayCanvas();
 void drawLine(int row,int col,int length);
 void drawRectangle(int row,int col,int width,int height);
+void drawTriangle(int row,int col,int size);
+void drawCircle(int centerX,int centerY,int radius);
 void initializeCanvas(){
     for(int i=0;i<30;i++){
         for(int j=0;j<60;j++){
@@ -35,13 +37,34 @@ void drawRectangle(int row,int col,int width,int height){
         canvas[row+i][col+width-1]='*';
     }
 }
+void drawTriangle(int row,int col,int size){
+    for(int i=0;i<size;i++){
+        canvas[row+i][col]='*';
+        canvas[row+size-1][col+i]='*';
+        canvas[row+i][col+i]='*';
+    }
+}
+void drawCircle(int centerX,int centerY,int radius){
+    for(int i=0;i<30;i++){
+        for(int j=0;j<60;j++){
+            int dx=i-centerX;
+            int dy=j-centerY;
+            int d=dx*dx+dy*dy;
+            if(d>=radius*radius-radius && d<=radius*radius+radius){
+                canvas[i][j]='*';
+            }
+
+        }
+    }
+}
 int main()
 
 {
     initializeCanvas();
     drawLine(2,5,10);
     drawRectangle(8,10,12,5);
-    printf("test\n");
+    drawTriangle(15,5,8);
+    drawCircle(15,45,4);
     displayCanvas();
     getchar();
     return 0;
