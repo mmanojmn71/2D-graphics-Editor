dd69306b9316cabdc72a6769a687abe1139451c2
Manoj MN <mmanojmn71@gmail.com>
2026-06-10 11:37:42 +0530

    Update graphics.c

---
+++ graphics.c
@@ -0,0 +1,14 @@
+include "graphics.h"
+#include <stdio.h>
+
+void drawLine() {
+    printf("Drawing line...\n");
+}
+
+void drawCircle() {
+    printf("Drawing circle...\n");
+}
+
+void clearCanvas() {
+    printf("Canvas cleared.\n");
+}
